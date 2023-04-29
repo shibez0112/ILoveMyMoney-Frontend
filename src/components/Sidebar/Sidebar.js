ec6e1d225f8343988a87104e33607ab081e74c08
shibez <ginta2888@gmail.com>
2023-04-29 23:31:49 +0700

    style: Minor change to side bar
    Remove menu icon and add profile icon

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
-import { MdMenu, MdAccountBalanceWallet, MdHelpOutline } from "react-icons/md";
+import { MdAccountBalanceWallet, MdHelpOutline } from "react-icons/md";
+import { BiUserCircle } from "react-icons/bi";
 import { TbReportAnalytics } from "react-icons/tb";
 import { React, useState } from "react";
 import { Menu } from "antd";
@@ -6,24 +7,24 @@ import { Menu } from "antd";
 
 const items = [
   {
-    label: "Navigation One",
-    key: "mail",
-    icon: <MdMenu size={25}/>,
-  },
-  {
     label: "Wallet",
     key: "wallet",
-    icon: <MdAccountBalanceWallet size={25}/>,
+    icon: <MdAccountBalanceWallet size={25} />,
   },
   {
     label: "Report",
     key: "report",
-    icon: <TbReportAnalytics size={25}/>,
+    icon: <TbReportAnalytics size={25} />,
   },
   {
     label: "Help",
     key: "help",
-    icon: <MdHelpOutline size={25}/>,
+    icon: <MdHelpOutline size={25} />,
+  },
+  {
+    label: "Profile",
+    key: "profile",
+    icon: <BiUserCircle size={25} />,
   },
 ];
 
